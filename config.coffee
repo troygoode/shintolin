e141cb3551f608adace22fa638897215e02ce853
Troy Goode <troygoode@gmail.com>
2015-07-02 07:53:39 -0400

    remove nodetime

--- config.coffee
+++ config.coffee
@@ -2,7 +2,6 @@ module.exports =
   port: process.env.PORT or 3000
   session_secret: process.env.SESSION_SECRET or 'secret'
   mongo_uri: process.env.MONGOLAB_URI or 'mongodb://localhost/shintolin'
-  nodetime_api_key: process.env.NODETIME_API_KEY
   maximum_cpus: process.env.MAXIMUM_CPUS or 1
 
   maximum_level: 18
--- server.js
+++ server.js
@@ -1,16 +1,9 @@
-require('coffee-script/register');
+/* global require */
 
-var config = require('./config');
+require("coffee-script/register");
 
-if(config.nodetime_api_key && config.nodetime_api_key.length){
-  require('nodetime').profile({
-    accountKey: config.nodetime_api_key,
-    appName: 'Shintolin',
-    debug: true
-  });
-}
-
-var cluster = require('./apps/cluster'),
-    app = require('./apps/website/app');
+var config = require("./config"),
+  cluster = require("./apps/cluster"),
+  app = require("./apps/website/app");
 
 cluster(app, config.maximum_cpus);
